c33966faaa3b070a802b46e5a29015e16ffc3c94
jadenfk@outlook.com <jadenfk@outlook.com>
2023-03-02 23:51:48 -0500

    New push to google sheets method

--- fin/src/main.js
+++ fin/src/main.js
@@ -6,9 +6,11 @@ import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
 import './assets/main.css'
 import 'bootstrap/dist/css/bootstrap.css';
 import "bootstrap-vue/dist/bootstrap-vue.css";
+import LoadScript from 'vue-plugin-load-script';
 
 Vue.use(BootstrapVue)
 Vue.use(BootstrapVueIcons)
+Vue.use(LoadScript)
 new Vue({
   router,
   render: (h) => h(App)
--- fin/src/views/FormView.vue
+++ fin/src/views/FormView.vue
@@ -1,22 +1,29 @@
 <template>
     <div>
         <b-container v-if="!done" fluid style="margin-top:20px">
-            <b-row class="text-center"><b-col><h5>Please review these works of art from {{ current_artist }}</h5></b-col></b-row>
+            <b-row class="text-center"><b-col>
+                    <h5>Please review these works of art from {{ current_artist }}</h5>
+                </b-col></b-row>
             <b-row align-h="center">
                 <b-col style="margin-top:20px" cols="auto" v-for="image in example_images">
-                    <b-img  v-b-popover.hover="{html:true, content:'<img style=\'height:512px;width:512px\' src=\'' + image + '\'></img>'}" :src="image" :height="256" :width="256"></b-img>
+                    <b-img
+                        v-b-popover.hover="{ html: true, content: '<img style=\'height:512px;width:512px\' src=\'' + image + '\'></img>' }"
+                        :src="image" :height="256" :width="256"></b-img>
                 </b-col>
             </b-row>
             <hr style=" border-top: 10px solid rgba(0, 0, 0, 1.0);">
             <b-row style="margin-top:20px">
                 <b-col>
-                    <b-row style="justify-content: center;"><h6>{{ study_images.length }} cases left. {{ Object.keys(this.images).length }} artists to go</h6></b-row>
+                    <b-row style="justify-content: center;">
+                        <h6>{{ study_images.length }} cases left. {{ Object.keys(this.images).length }} artists to go</h6>
+                    </b-row>
                     <b-row style="justify-content: center;">
                         <b-img :src="study_image" :height="512" :width="512">
                         </b-img>
                     </b-row>
                     <b-row style="justify-content: center;">
-                        <p style="font-size:120%;">How confident are you that this image is from {{ current_artist }}? <br> (1 being least
+                        <p style="font-size:120%;">How confident are you that this image is from {{ current_artist }}? <br>
+                            (1 being least
                             confident and 5 being most confident)</p>
                     </b-row>
                     <b-row style="justify-content: center;">
@@ -44,19 +51,25 @@
                         </b-button-group></b-row>
                 </b-col>
             </b-row>
-            <b-row style="justify-content: center;"><b-button style="width:90px; height:60px; margin-top: 30px; margin-bottom: 50px;"
-                    @click="submit_rating" variant="primary">Next</b-button></b-row>
+            <b-row style="justify-content: center;"><b-button
+                    style="width:90px; height:60px; margin-top: 30px; margin-bottom: 50px;" @click="submit_rating"
+                    variant="primary">Next</b-button></b-row>
 
         </b-container>
         <div v-if="done" style="width:100vw; height:100vh; background-image: url('gj.jpg')"></div>
     </div>
 </template>
+
 <script>
 
 import axios from 'axios'
 import fs from '../assets/fs.json'
+import Vue from 'vue'
 
-let sheets_path = 'https://api.apispreadsheets.com/data/lyJ97n9V3CjwX24y/'
+const SHEET_ID = "1xD3BjxdUqxo9PJKh1zfWcHnsE9HugH8Sb1dpBIhPDkA";
+const API_KEY =
+    "AIzaSyCJRbZ98MM1rYZH-GQ_1USj5OmEeZ6STiQ";
+
 export default {
     name: 'Form',
     data() {
@@ -68,7 +81,7 @@ export default {
             rating: null,
             done: false,
             images: fs,
-            case_numer:1,
+            case_numer: 1,
             id: this.CreateUUID()
         }
     },
@@ -79,12 +92,48 @@ export default {
             if (this.rating != null) {
                 let value_body = { "uuid": this.id, "artist": this.current_artist, "image": this.study_image, "rating": this.rating }
 
-                axios.post(sheets_path, value_body).then((response) => {
-                    console.log(response)
+                var body = JSON.stringify({
+                    requests: [
+                        {
+                            appendCells: {
+                                rows: [
+                                    {
+                                        values: [
+                                            {
+                                                userEnteredValue: {
+                                                    stringValue: this.id,
+                                                },
+                                            },
+                                            {
+                                                userEnteredValue: {
+                                                    stringValue: this.current_artist,
+                                                },
+                                            },
+                                            {
+                                                userEnteredValue: {
+                                                    stringValue: this.study_image,
+                                                },
+                                            },
+                                            {
+                                                userEnteredValue: {
+                                                    numberValue: this.rating,
+                                                },
+                                            }
+                                        ],
+                                    },
+                                ],
+                                fields: "*",
+                            },
+                        },
+                    ],
                 })
-                    .catch((error) => {
-                        console.error(error)
-                    });
+
+                gapi.client.sheets.spreadsheets.batchUpdate({ spreadsheetId: SHEET_ID }, body).then(function (response) {
+                    // TODO: Change code below to process the `response` object:
+                    console.log(response.result);
+                }, function (reason) {
+                    console.error('error: ' + reason.result.error.message);
+                });
 
                 this.rating = null
 
@@ -105,7 +154,7 @@ export default {
 
             let artist_names = Object.keys(this.images)
 
-            if (artist_names.length > 0){
+            if (artist_names.length > 0) {
 
                 this.current_artist = artist_names[this.getRandomInt(artist_names.length)]
 
@@ -122,17 +171,17 @@ export default {
                 let SDNG_from = []
 
                 for (const type_key in this.images[this.current_artist]) {
-                    if (type_key.startsWith('SLD_from')){
+                    if (type_key.startsWith('SLD_from')) {
                         SLD_from.push(...this.images[this.current_artist][type_key])
                     }
-                    else if (type_key.startsWith('SDNG_from')){
+                    else if (type_key.startsWith('SDNG_from')) {
                         SDNG_from.push(...this.images[this.current_artist][type_key])
                     }
-                    else if (type_key.startsWith('from')){
+                    else if (type_key.startsWith('from')) {
 
                         continue
                     }
-                    else{
+                    else {
                         this.study_images.push(...this.shuffle(this.images[this.current_artist][type_key]).slice(0, 5))
                     }
                 }
@@ -148,7 +197,7 @@ export default {
 
             }
 
-            else{
+            else {
                 this.done = true
             }
 
@@ -183,8 +232,39 @@ export default {
 
     mounted() {
 
-        this.init_new_artist()
+        Vue.loadScript('https://apis.google.com/js/api.js').then(() => {
+            Vue.loadScript('https://cdnjs.cloudflare.com/ajax/libs/jsencrypt/3.0.0-rc.1/jsencrypt.min.js').then(() => {
+                Vue.loadScript('https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.0.0/crypto-js.min.js').then(() => {
+                    Vue.loadScript('https://cdn.jsdelivr.net/gh/tanaikech/GetAccessTokenFromServiceAccount_js@master/getaccesstokengromserviceaccount_js.min.js').then(() => {
+                        gapi.load('client', () => init(this));
 
+                        
+                        async function init(ths) {
+                            const object = {
+                                private_key: "-----BEGIN PRIVATE KEY-----\nMIIEugIBADANBgkqhkiG9w0BAQEFAASCBKQwggSgAgEAAoIBAQCg+cafX2D1o1zc\n49kW3Mxpx/1dOk4T0UB76M0pQPVdksJkYMFemPTKLPJcMb7U3lNEXJ5aowRlbAsN\ng+9J+QQ3A/EO1qirEjAO4f7VV7EqagUdY442ybNnBq2iqGZbAOsTQRWqvkqMdVEQ\nC8UPPZXN/0HrXEq7xFDnSudjhi0/xNMvIaFfWR2YOOoM/zB47+Zqmcf+9zlsXMWz\nFAHJ4OiKH/t1tXm1FShSquo/XfxC5grO6Vr5ADZhgCxEpD5LT+f8ry9G726PUyFx\nJiMUX6KM5px6IRU/wIPQy4z/p7Rso5zeeCbeU8FUYjkDJ0Ef5jDDwbOK/aCJBKQn\n27Ibht65AgMBAAECgf9q1z7meVDoxvVql1b+r/b8PnyYPkAbjL9HYEPaFh15FFDx\nMFW+AorQeASB8CgLL97X8ZH7eCgB3X88RHqkHjHEjjsOeIi2a662MgzajI4RggE9\nabCYQxnmEr1ZIYoZJ8aiCGr3LBlOmK0AHwvvc3+nQTTwNkm+KOERGryH/vcngIOT\nJJRBSrWRAXYZUqZhQBNi57TEhdS4pbkH946A1WdK+Ex7SXEJDH8cYuaFyPbtXATm\nxf+UcdMw+ftBrQ588Y8W7RSmiqxzMXMfCayuU1ELGl3dZMFbAYckikdVHNUBZcXt\nAqJPVSiPEOkHtsuX/QKXArgYXqPRiLioL3AH8UECgYEA3GecPeREgOKr3v2vMZsd\nbj3Vsp/CXcX9DiObjO9f83i9J2iwGGGAndb7FP1sFUX2eyW4decQixaIKyaS5D6q\nHydTwgdlaF9cpduGvkgaHO9fdiWGPCCLZVQNhaXLrGuU2AIcUwgYvPZ7RHO56uzg\nuZAiczSPjFghf6zy+xY5FBECgYEAuvkjOTn05J9KtUnG9RPyTyf28v11zmMMwe1D\nMS/iXcSPRRRWMPAcUHUgrswwoB73y7YtXiJrDH1MS2P6om6rTBW5HYBGOSkeHg1V\nKnY25vLR6NPKtAK4EHtPc5+6jFL1OS8yb97J+fB2tgWOqGFu/03TE2vgfzww5bXD\n7M1AKCkCgYBx0b/J5nvJ40TGn1M91tDYAW6+E+qX6orIJnNJ0MRIQ23LUDDAJnn5\nCPAY4tZSFhM3fT9JA85zRWuEfSMWY5YPCIwkgxVHVRfPPxLSaQGzN5qgtLghjNzv\nsvxYrEoF/HidJFnkNyYvT0pRC0JTo4hZy6k+vUH0Q/7opDPGnq9TQQKBgC9+6ju3\nRUZuUIQM4cWP85W4CTtgt3ReJPd+VftAtrgrHlnl+J8l34twtYSxyghi1X+iH5Qy\nGg9qJN9h3mE0y77gJc4gr7YFg+kOafwHW5r/Q+NpKazgo/FL4br1cGYFDNYh7yYL\n6jLbqsxCTSLo3VFta5kbZkPr/l/b9JFK8mlBAoGAP1LhOtRDpH/iZQ8z7JoOKYsm\n1ZnIpMHIpUwpX4kMbrK85RROXDBdPjIImLEEr3AxB3TrGS6jfDOCeDwevAli+7fa\nFmRUoqCwZm3I6t7rsbrrAzaf4PG0kPNMUkHNQcSjjZHmILYvY7i+lKn+UBGMilad\n0cDqX0wptOZb2TFamHE=\n-----END PRIVATE KEY-----\n",
+                                client_email: "dmst-service@demyst-379323.iam.gserviceaccount.com",
+                                scopes: ["https://www.googleapis.com/auth/spreadsheets"],
+                            };
+
+                            gapi.auth.setToken(await GetAccessTokenFromServiceAccount.do(object))
+                            gapi.client.init({
+                                discoveryDocs: ["https://sheets.googleapis.com/$discovery/rest?version=v4"]
+                            }).then(() => {
+                                ths.init_new_artist()
+                            })
+
+                            
+                        }
+
+                        
+
+
+                    })
+
+                })
+
+            })
+        })
     }
 
 }
